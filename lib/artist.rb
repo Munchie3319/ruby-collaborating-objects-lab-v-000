584cd93f091e9cb9c9a3959a1f7610b08c68bd42
rosa lee <rosalee0116@gmail.com>
2020-04-13 00:50:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor: name 
+  attr_accessor: name
 
   @@all = []
 
@@ -8,22 +8,22 @@ class Artist
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
   def add_song(song)
-    song.artist = self 
+    song.artist = self
   end
 
   def songs
-    Song.all.select{|artist| artist.name == name} || 
+    Song.all.select{|artist| artist.name == name} ||
     Artist.new(name)
   end
 
   def print_songs
     songs.each do |song|
-      puts song.name 
+      puts song.name
     end
   end
 end
