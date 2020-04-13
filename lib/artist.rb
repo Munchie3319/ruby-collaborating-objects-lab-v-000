bb1b0908dd89ba75ebf6f2bd60448aeae876234f
rosa lee <rosalee0116@gmail.com>
2020-04-13 00:33:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,5 @@
 class Artist
-  attr_accessor :name, :songs, :all
-
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
@@ -8,7 +7,39 @@ class Artist
     @songs = []
   end
 
+  def self.all 
+    @@all 
+  end
+
+  def add_song(song)
+    @songs << song 
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|artist| artist.name = name}
+  end
+
+  def self.create_by_name(name)
+    Artist.new(name).tap{|artist| artist.save}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+
   def add_song(song)
     @songs << song
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def print_songs
+    @songs.each{|song| puts song.name}
   end
 end
