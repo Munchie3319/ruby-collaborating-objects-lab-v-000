2580fbc29e4a712b24270ea49faa2e2b2e6047df
rosa lee <rosalee0116@gmail.com>
2020-04-13 00:58:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,29 @@
 class Song
    attr_accessor :name, :artist
 
+   @@all = [ ]
+
     def initialize(name)
      @name = name
+     @artist = artist
+     @@all << self 
    end
 
+    def self.all
+      @@all 
+    end 
+    
     def self.new_by_filename(filename)
-     artist = filename.split(" - ")[0]
-     song = filename.split(" - ")[1]
-     song = Song.new(song)
-     song.artist_name = artist
+     artist_name = filename.split(" - ")[0]
+     song_name = filename.split(" - ")[1]
+     song = Song.new(song_name)
+     song.artist_name = artist_name 
      song
    end
 
     def artist_name=(name)
-     self.artist = Artist.find_or_create_by_name(name)
+     artist = Artist.find_or_create_by_name(name)
+     self.artist =artist 
      artist.add_song(self)
    end
  end
